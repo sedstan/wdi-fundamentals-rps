a0ce4f70506840082a3cfd8ce24fd3a92dba9541
S.S.Said <sedky.stanley.said@gmail.com>
2015-09-27 02:44:19 +0100

    created new getWinner code

--- app/js/rockpaperscissors.js
+++ app/js/rockpaperscissors.js
@@ -21,18 +21,19 @@ function randomPlay() {
 /*           Write Your Code Below            */
 ////////////////////////////////////////////////
 
-function getPlayerMove(move) {
+function getPlayerMove(move) { 
+     
     // Write an expression that operates on a variable called `move`
     // If a `move` has a value, your expression should evaluate to that value.
     // However, if `move` is not specified / is null, your expression should equal `getInput()`.
-    return /* Your Expression */;
+    return (move) ? getInput: move;
 }
 
 function getComputerMove(move) {
     // Write an expression that operates on a variable called `move`
     // If a `move` has a value, your expression should evaluate to that value.
     // However, if `move` is not specified / is null, your expression should equal `randomPlay()`.
-    return /* Your Expression */;
+    return (move) ? randomPlay : move;
 }
 
 function getWinner(playerMove,computerMove) {
@@ -40,7 +41,38 @@ function getWinner(playerMove,computerMove) {
     // Write code that will set winner to either 'player', 'computer', or 'tie' based on the values of playerMove and computerMove.
     // Assume that the only values playerMove and computerMove can have are 'rock', 'paper', and 'scissors'.
     // The rules of the game are that 'rock' beats 'scissors', 'scissors' beats 'paper', and 'paper' beats 'rock'.
-    /* YOUR CODE HERE */
+  if ( playerMove===computerMove) {
+    return "The result is a tie!";
+ }
+ else if (playerMove==="rock"){
+    
+    if(computerMove==="scissors"); {
+        
+        return "rock wins";
+    } 
+    { "paper wins";
+ }
+ if (playerMove===computerMove) {
+    return "The result is a tie!";
+ }
+ else if (playerMove==="scissors") {
+    
+    if(computerMove==="paper"); {
+        
+        return "scissors wins";
+    }
+     "rock wins";
+ }
+if (playerMove===computerMove){
+    return "The result is a tie!";
+} 
+else if (playerMove==="paper") {
+    
+    if(computerMove==="rock");
+return "paper wins";
+}
+else "scissors win";
+}
     return winner;
 }
 
